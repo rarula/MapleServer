36cff65c837af3266ffe6e76b96e189087039bd5
raru <74240663+raruData@users.noreply.github.com>
2022-05-24 00:27:11 +0900

    使用例を作成

---
+++ examples/example.js
@@ -0,0 +1,80 @@
+const { MapleServer } = require('../lib');
+
+const server = new MapleServer({
+    rconClient: {
+        port: 25575,
+        host: 'localhost',
+        password: 'password',
+    },
+    server: {
+        directoryPath: 'path',
+    },
+});
+
+server.start(['-Xmx2G', '-Xms2G', '-jar', 'server.jar']);
+
+server.on('playerChat', async (sender, message) => {
+    if (message === 'ping') {
+        const text = [
+            {
+                text: ' -> ',
+                color: 'gray',
+            },
+            {
+                text: 'pong!',
+                color: 'gold',
+            },
+        ];
+        sender.sendCommand(`tellraw @s ${JSON.stringify(text)}`);
+        return;
+    }
+
+    if (message === 'date') {
+        const text = [
+            {
+                text: ' -> ',
+                color: 'gray',
+            },
+            {
+                text: `${new Date().toLocaleString('en-US')}`,
+                color: 'gold',
+            },
+        ];
+        sender.sendCommand(`tellraw @s ${JSON.stringify(text)}`);
+        return;
+    }
+
+    if (message === 'info') {
+        const properties = server.getProperties();
+        const level = await sender.mapleWorld.getLevel();
+        const players = await sender.mapleWorld.getOnlinePlayers();
+        const text = [
+            {
+                text: ' -> flavor: ',
+                color: 'gray',
+            },
+            {
+                text: `${level.Data.ServerBrands}`,
+                color: 'gold',
+            },
+            {
+                text: ', version: ',
+                color: 'gray',
+            },
+            {
+                text: `${level.Data.Version.Name}`,
+                color: 'gold',
+            },
+            {
+                text: ', players: ',
+                color: 'gray',
+            },
+            {
+                text: `${players.length} / ${properties['max-players']}`,
+                color: 'gold',
+            },
+        ];
+        sender.sendCommand(`tellraw @s ${JSON.stringify(text)}`);
+        return;
+    }
+});
